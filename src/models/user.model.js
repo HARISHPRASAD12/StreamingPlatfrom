19b5ca1e17688f97cd812b5495d097178ab95817
Harishprsd <harish.prsd4@gmail.com>
2026-02-14 00:06:26 +0530

    models added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,41 @@
 import mongoose, { Schema } from "mongoose";
 
 const UserSchema = new Schema({
-    
-})
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //cloudinary url
+        required: true
+    },
+    avatar: {
+        type: String, //cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+
+    ],
+    password: {
+        type: String,
+        required: [true, 'password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+}, { timestamps: true })
 
 
-export const User =mongoose.model("User", UserSchema)
+export const User = mongoose.model("User", UserSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const VideoSchema=new Schema({
+    videoFile:{
+        type:String,//cloudeinary url
+        required:true
+    },
+    thumnail:{
+        type:String,//cloudeinary url
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,//cloudeinary url
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+},{timestamps:true})
+
+export const Video=mongoose.model("Video",VideoSchema)
