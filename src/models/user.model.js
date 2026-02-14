3234ffe249df1f230de6b5a35b1a36571b02a7f7
Harishprsd <harish.prsd4@gmail.com>
2026-02-15 00:42:34 +0530

    add user and viedo model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -39,15 +39,41 @@ const UserSchema = new Schema({
     }
 }, { timestamps: true })
 
-UserSchema.pre("save", async function (next){
-    if(!this.isModified("password")) return next();
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
-UserSchema.methods.isPasswordCorrect = async function(password){
+UserSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
+
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+ }
 
 export const User = mongoose.model("User", UserSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,42 +1,42 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const VideoSchema=new Schema({
-    videoFile:{
-        type:String,//cloudeinary url
-        required:true
+const VideoSchema = new Schema({
+    videoFile: {
+        type: String,//cloudeinary url
+        required: true
     },
-    thumnail:{
-        type:String,//cloudeinary url
-        required:true
+    thumnail: {
+        type: String,//cloudeinary url
+        required: true
     },
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    duration:{
-        type:Number,//cloudeinary url
-        required:true
+    duration: {
+        type: Number,//cloudeinary url
+        required: true
     },
-    views:{
-        type:Number,
-        default:0
+    views: {
+        type: Number,
+        default: 0
     },
-    isPublished:{
-        type:Boolean,
-        default:true
+    isPublished: {
+        type: Boolean,
+        default: true
     },
-    owner:{
+    owner: {
         type: Schema.Types.ObjectId,
         ref: "User"
     }
 
-},{timestamps:true})
+}, { timestamps: true })
 
 VideoSchema.plugin(mongooseAggregatePaginate)
 
-export const Video=mongoose.model("Video",VideoSchema)
+export const Video = mongoose.model("Video", VideoSchema)
