562e9383d96e174faef96ece648f43231caa873c
Harishprsd <harish.prsd4@gmail.com>
2026-03-25 00:10:53 +0530

    add cloudinarypath

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,2 +1,18 @@
 import { v2 as cloudinary} from "cloudinary";
 import fs from "fs"; 
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) {
+
+
+cloudinary.v2.uploader.upload("",{public_id: ""}, 
+    function(error, result) {
+    console.log(result, error);
+});
