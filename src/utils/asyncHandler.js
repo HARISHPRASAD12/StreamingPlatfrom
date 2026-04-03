fef9d714e4a4f9615e0781839f07797b1b9c9be4
Harishprsd <harish.prsd4@gmail.com>
2026-04-03 19:04:53 +0530

    all setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,11 @@
 const asyncHandler = (requesthandler) => {
-    (req, res, next) => {
+   return (req, res, next) => {
         Promise.resolve(requesthandler(req, res, next)).catch((err) => next(err))
 
     }
 }
 
 export { asyncHandler }
-
-
 
 
 // const asyncHandler =(func)=>()=>{}
